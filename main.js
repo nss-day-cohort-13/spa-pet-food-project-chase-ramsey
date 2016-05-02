00f73b9e1c319761064167b5d03b2c227ee3b3a1
Chase Ramsey <ramsey.chase@gmail.com>
2016-05-02 13:41:17 -0500

    Created new branch for assignment

--- main.js
+++ main.js
@@ -8,7 +8,13 @@ requestDogs.addEventListener("load", printDogCards);
 requestDogs.open("GET", "dogs.json");
 requestDogs.send();
 
-// Create a function that formats cards
+// Create a request for cats.json
+var requestCats = new XMLHttpRequest();
+requestCats.addEventListener("load", printCatCards);
+requestCats.open("GET", "cats.json");
+requestCats.send();
+
+// Create a function that formats dog cards
 function buildDogCards(dataArray) {
 	for (var i = 0; i < dataArray.length; i++) {
 		var current = dataArray[i];
@@ -31,6 +37,30 @@ function buildDogCards(dataArray) {
  	}
 }
 
+// Create a function that formats cat cards
+
+function buildCatCards(dataArray) {
+	for (var i = 0; i < dataArray.length; i++) {
+		var current = dataArray[i];
+		outputDiv.innerHTML += "<article class='card'>" +
+													 "<header class='card-header'>" +
+													 "<h3>" + current.name + "</h3>" + "</header>" +
+													 "<div class='card-body'>" +
+													 "<h4>" + current.breeds + "</h4>" +
+													 "<p>" + current.types[0].type + "<p>" +
+													 "<p>" + current.types[0].volumes[0].name + "</p>" +
+													 "<p>" + current.types[0].volumes[0].price + "<p>" +
+													 "</div></article>" +
+													 "<article class='card'>" +
+													 "<header class='card-header'>" +
+													 "<h3>" + current.name + "</h3>" + "</header>" +
+													 "<div class='card-body'>" +
+													 "<p>" + current.types[1].type + "<p>" +
+													 "<p>" + current.types[1].volumes[1].name + "</p>" +
+													 "<p>" + current.types[1].volumes[1].price + "<p>" +
+													 "</div></article>";
+ 	}
+}
 
 // Create a function that parses data and prints to the DOM
 function printDogCards() {
@@ -38,3 +68,12 @@ function printDogCards() {
 	console.log("dataDogFood: ", dataDogFood);
 	buildDogCards(dataDogFood.dog_brands);
 }
+
+// Create a function that parses the data for cats.json and prints to the DOM
+
+function printCatCards() {
+	var dataCatFood = JSON.parse(this.responseText);
+	console.log("dataCatFood: ", dataCatFood);
+	buildCatCards(dataCatFood.cat_brands);
+}
+
