a0f70de50665cf8d1de365dd3d2fe040ded57c6e
Chase Ramsey <ramsey.chase@gmail.com>
2016-05-02 15:13:32 -0500

    Factored and reworked both the dog and cat food loops

--- main.js
+++ main.js
@@ -16,56 +16,57 @@ requestCats.send();
 
 // Create a function that formats dog cards
 function buildDogCards(dataArray) {
+	var cardsHTML = "";
 	for (var i = 0; i < dataArray.length; i++) {
 		var current = dataArray[i];
-		outputDiv.innerHTML += "<article class='card'>" +
-													 "<header class='card-header'>" +
-													 "<h3>" + current.name + "</h3>" + "</header>" +
-													 "<div class='card-body'>" +
-													 "<p>" + current.types[0].type + "<p>" +
-													 "<p>" + current.types[0].volumes[0].name + "</p>" +
-													 "<p>" + current.types[0].volumes[0].price + "<p>" +
-													 "</div></article>" +
-													 "<article class='card'>" +
-													 "<header class='card-header'>" +
-													 "<h3>" + current.name + "</h3>" + "</header>" +
-													 "<div class='card-body'>" +
-													 "<p>" + current.types[1].type + "<p>" +
-													 "<p>" + current.types[1].volumes[1].name + "</p>" +
-													 "<p>" + current.types[1].volumes[1].price + "<p>" +
-													 "</div></article>";
+		cardsHTML += "<article class='card'>" +
+								 "<header class='card-header'>" +
+								 "<h3>" + current.name + "</h3>" + "</header>" +
+								 "<div class='card-body'>";
+		 for (var t = 0; t < current.types.length; t++) {
+		 	cardsHTML += "<section class='type'>" +
+									 "<p>" + current.types[t].type + "<p>";
+				for (var v = 0; v < current.types[t].volumes.length; v++) {
+					 cardsHTML += "<p>" + current.types[t].volumes[v].name + "</p>" +
+					 							"<p>" + current.types[t].volumes[v].price + "<p>";
+					}
+		 cardsHTML += "</section>"
+		 }
+
+		cardsHTML += "</div></article>";
  	}
+ 	outputDiv.innerHTML += cardsHTML;
 }
 
 // Create a function that formats cat cards
 
 function buildCatCards(dataArray) {
+	var cardsHTML = "";
 	for (var i = 0; i < dataArray.length; i++) {
 		var current = dataArray[i];
-		outputDiv.innerHTML += "<article class='card'>" +
-													 "<header class='card-header'>" +
-													 "<h3>" + current.name + "</h3>" + "</header>" +
-													 "<div class='card-body'>" +
-													 "<h4>" + current.breeds + "</h4>" +
-													 "<p>" + current.types[0].type + "<p>" +
-													 "<p>" + current.types[0].volumes[0].name + "</p>" +
-													 "<p>" + current.types[0].volumes[0].price + "<p>" +
-													 "</div></article>" +
-													 "<article class='card'>" +
-													 "<header class='card-header'>" +
-													 "<h3>" + current.name + "</h3>" + "</header>" +
-													 "<div class='card-body'>" +
-													 "<p>" + current.types[1].type + "<p>" +
-													 "<p>" + current.types[1].volumes[1].name + "</p>" +
-													 "<p>" + current.types[1].volumes[1].price + "<p>" +
-													 "</div></article>";
+		cardsHTML += "<article class='card'>" +
+								 "<header class='card-header'>" +
+								 "<h3>" + current.name + "</h3>" + "</header>" +
+								 "<div class='card-body'>" +
+								 "<h4>" + current.breeds + "</h4>";
+		 for (var t = 0; t < current.types.length; t++) {
+		 	cardsHTML += "<section class='type'>" +
+	  							 "<p>" + current.types[t].type + "<p>";
+				for (var v = 0; v < current.types[t].volumes.length; v++) {
+					 cardsHTML += "<p>" + current.types[t].volumes[v].name + "</p>" +
+					 							"<p>" + current.types[t].volumes[v].price + "<p>";
+					}
+		 cardsHTML += "</section>"
+		 }
+
+		cardsHTML += "</div></article>";
  	}
+ 	outputDiv.innerHTML += cardsHTML;
 }
 
 // Create a function that parses data and prints to the DOM
 function printDogCards() {
 	var dataDogFood = JSON.parse(this.responseText);
-	console.log("dataDogFood: ", dataDogFood);
 	buildDogCards(dataDogFood.dog_brands);
 }
 
@@ -73,7 +74,6 @@ function printDogCards() {
 
 function printCatCards() {
 	var dataCatFood = JSON.parse(this.responseText);
-	console.log("dataCatFood: ", dataCatFood);
 	buildCatCards(dataCatFood.cat_brands);
 }
 
