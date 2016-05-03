891a7b1bc4c393917621928ca00bc96843e31ded
Chase Ramsey <ramsey.chase@gmail.com>
2016-05-03 09:25:00 -0500

    Refactored and streamlined main.js

--- main.js
+++ main.js
@@ -15,36 +15,8 @@ requestCats.open("GET", "cats.json");
 requestCats.send();
 
 // Create a function that formats dog cards
-function buildDogCards(dataArray) {
+function buildCards(dataArray) {
 	var cardsHTML = "";
-	for (var i = 0; i < dataArray.length; i++) {
-		var current = dataArray[i];
-		cardsHTML += "<article class='card'>" +
-								 "<header class='card-header'>" +
-								 "<h3>" + current.name + "</h3>" +
-								 "<h5>" + "All breeds" + "</h5>" + "</header>" +
-								 "<div class='card-body'>";
-		 for (var t = 0; t < current.types.length; t++) {
-		 	cardsHTML += "<section class='type'>" +
-									 "<div class='type-title'>" + current.types[t].type + "</div>";
-				for (var v = 0; v < current.types[t].volumes.length; v++) {
-					 cardsHTML += "<div class='type-data'>" +
-					 							"<p>" + current.types[t].volumes[v].name + "</p>" +
-					 							"<p>" + current.types[t].volumes[v].price + "<p>" +
-					 							"</div>";
-					}
-		 cardsHTML += "</section>"
-		 }
-
-		cardsHTML += "</div></article>";
- 	}
- 	outputDiv.innerHTML += cardsHTML;
-}
-
-// Create a function that formats cat cards
-
-function buildCatCards(dataArray) {
-var cardsHTML = "";
 	for (var i = 0; i < dataArray.length; i++) {
 		var current = dataArray[i];
 		cardsHTML += "<article class='card'>" +
@@ -69,16 +41,17 @@ var cardsHTML = "";
  	outputDiv.innerHTML += cardsHTML;
 }
 
+
 // Create a function that parses data and prints to the DOM
 function printDogCards() {
 	var dataDogFood = JSON.parse(this.responseText);
-	buildDogCards(dataDogFood.dog_brands);
+	buildCards(dataDogFood.dog_brands);
 }
 
 // Create a function that parses the data for cats.json and prints to the DOM
 
 function printCatCards() {
 	var dataCatFood = JSON.parse(this.responseText);
-	buildCatCards(dataCatFood.cat_brands);
+	buildCards(dataCatFood.cat_brands);
 }
 
